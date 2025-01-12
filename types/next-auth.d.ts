d6c382bfdc40c3671985301b9921ea9f49215416
Abhinav Kale <133649888+Abhi16016@users.noreply.github.com>
2025-01-13 04:07:48 +0530

    Update next-auth.d.ts

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -2,15 +2,15 @@ import NextAuth from "next-auth";
 
 declare module "next-auth" {
   interface User {
-    avatar?: string | null; // Add the avatar property
+    avatar?: string | null; 
   }
 
   interface Session {
     user: {
-      id?: string | null; // Optional: Add ID if used in your session
+      id?: string | null;
       name?: string | null;
       email?: string | null;
-      avatar?: string | null; // Add the avatar property
+      avatar?: string | null;
     };
   }
 }
